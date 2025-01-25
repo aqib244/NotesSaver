a4d5601bce9de5edbe25d213790dd454165a02d4
aqib244 <aqibm8383@gmail.com>
2025-01-25 10:18:07 +0500

    fixed localstorage

--- src/redux/notesSlice.js
+++ src/redux/notesSlice.js
@@ -1,42 +1,42 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
 import toast from 'react-hot-toast';
+
 const initialState = {
-  notes : localStorage.getItem("note") ? JSON.parse("note") : []  
-}
+  notes: localStorage.getItem("notes") ? JSON.parse(localStorage.getItem("notes")) : []  
+};
+
 export const notesSlice = createSlice({
   name: 'notes',
   initialState,
   reducers: {
-    addNote: (state , action ) => {
+    addNote: (state, action) => {
       const note = action.payload;
-      state.notes.push(note)
-      localStorage.setItem("notes" , JSON.stringify(state.notes))
-     toast.success("Note Added Succesfully")
+      state.notes.push(note);
+      localStorage.setItem("notes", JSON.stringify(state.notes)); // Save to localStorage
+      toast.success("Note Added Successfully");
     },
-    updateNote: (state , action ) => {
-      const note = action.payload
-      const i = state.notes.findIndex((item)=> item.id === note.id)
-      if (i>=0)
-        {
-          state.notes[i] = note;
-          localStorage.setItem("notes" , JSON.stringify(state.notes))
-        }
-        toast.success("Updated Successfully")
+    updateNote: (state, action) => {
+      const note = action.payload;
+      const i = state.notes.findIndex((item) => item.id === note.id);
+      if (i >= 0) {
+        state.notes[i] = note;
+        localStorage.setItem("notes", JSON.stringify(state.notes)); // Save to localStorage
+      }
+      toast.success("Updated Successfully");
     },
-  
     deleteNote: (state, action) => {
-      const noteId = action.payload
-      const i = state.notes.findIndex((item)=> item.id === noteId)
-      if(i>=0){
-        state.notes.splice(i , 1)
-        localStorage.setItem("notes" , JSON.stringify(state.notes))
-        toast.success("Note Deleted Successfully")
+      const noteId = action.payload;
+      const i = state.notes.findIndex((item) => item.id === noteId);
+      if (i >= 0) {
+        state.notes.splice(i, 1);
+        localStorage.setItem("notes", JSON.stringify(state.notes)); // Save to localStorage
+        toast.success("Note Deleted Successfully");
       }
     }
   }
-})
+});
 
 // Action creators are generated for each case reducer function
-export const { addNote, updateNote, deleteNote } = notesSlice.actions
+export const { addNote, updateNote, deleteNote } = notesSlice.actions;
 
-export default notesSlice.reducer
+export default notesSlice.reducer;
